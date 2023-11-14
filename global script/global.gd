39be51431d5b7bbfd3f5b453535cc0b4ab25e464
tzjingg <135602156+tzjingg@users.noreply.github.com>
2023-11-14 09:49:48 +0800

    Add files via upload

---
+++ global script/global.gd
@@ -0,0 +1,15 @@
+extends Node
+
+var next_scene = 0
+var key_collected = 0
+var player_pos
+var time
+var laser_hit = 0
+var trampoline_touched = 0
+var trampoline_rotation
+var door_entered = 0
+var jumpscare = 0
+var lock_opened = 0
+var ended = 0
+var caged = 0
+var dead = 0
